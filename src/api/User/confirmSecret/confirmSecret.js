c50eee2f48d21c320d136dd0aeac7aad9e4d7dd1
sunjae <azuretic94@gmail.com>
2020-01-01 21:12:32 +0900

    로그인 완료 시 확인 문자 초기화

--- src/api/User/confirmSecret/confirmSecret.js
+++ src/api/User/confirmSecret/confirmSecret.js
@@ -7,7 +7,15 @@ export default {
 
             const {email, secret} = args;
             const user = await prisma.user({email});
+            //로그인이 되면
             if(user.loginSecret === secret){
+                //로그인 확인 문자 초기화
+                await prisma.updateUser({
+                where:{id: user.id},
+                  data:{
+                      loginSecret: ""
+                      }
+                });
                 //JWT 토큰 생성
                 return generateToken(user.id);          
             }
