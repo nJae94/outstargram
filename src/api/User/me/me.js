01859e0a2773e879b37c27c0f2cfa1e70b3f5d25
sunjae <azuretic94@gmail.com>
2020-01-02 23:23:48 +0900

    자신의 프로필에서 풀 네임 가져오는거 수정

--- src/api/User/me/me.js
+++ src/api/User/me/me.js
@@ -1,5 +1,4 @@
 import { prisma } from "../../../../generated/prisma-client";
-import { USER_FRAGMENT } from "../../../fragments";
 
 export default {
   Query: {
@@ -7,11 +6,19 @@ export default {
       isAuthenticated(request);
       const { user } = request;
       const userProfile = await prisma.user({ id: user.id });
+      // 유저 아이디에 해당되는 posts정보를 가져옴
       const posts = await prisma.user({ id: user.id }).posts();
       return {
         user: userProfile,
         posts
       };
+    }
+  },
+  //parent는 User를 호출 하는 상위 resolver 여기선 user
+  User: {
+    fullName: parent => {
+      // parent에서 id에 해당하는 user정보를 가져오니까 그 중 firstName과 lastName을 합쳐서 반환
+      return `${parent.firstName} ${parent.lastName}`;
     }
   }
 };
