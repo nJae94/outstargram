90b8c4d464082293a483fbfd413a151021ca5622
sunjae <azuretic94@gmail.com>
2020-01-11 22:51:17 +0900

    계정 생성 시 user를 반환하는게 아니라 t/f값만 반환하도록 변경

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -6,7 +6,7 @@ export default {
             const {username, email, firstName="", lastName="", bio=""} = args;
             const user = await prisma.createUser({username,email,firstName,lastName,bio});
 
-            return user;
+            return true;
         }
     }
 }
