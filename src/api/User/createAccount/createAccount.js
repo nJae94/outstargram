b412d3e3e538571c9a3afa9173e0192d396794d1
sunjae <azuretic94@gmail.com>
2020-01-12 22:30:52 +0900

    로그인 시 중복된 값 입력 방지 예외처리 추가

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -4,7 +4,22 @@ export default {
     Mutation: {
         createAccount: async(_,args)=> {
             const {username, email, firstName="", lastName="", bio=""} = args;
-            const user = await prisma.createUser({username,email,firstName,lastName,bio});
+            // DB안에 입력된 Email과 username이 같은 값이 있는지 확인
+            const exists = await prisma.$exists.user({
+                OR: [
+                  {
+                    username
+                  },
+                  { email }
+                ]
+              });
+
+                //있다면 에러
+              if (exists) {
+                throw Error("동일 uesrname과 email이 있습니다.");
+              }
+
+            await prisma.createUser({username,email,firstName,lastName,bio});
 
             return true;
         }
