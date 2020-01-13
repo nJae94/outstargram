f191154db5c1291c0bb2674f1d51bd88bec5ae5c
sunjae <azuretic94@gmail.com>
2020-01-13 23:20:45 +0900

    user profile이 아니라 그냥 user로 가져오도록 변경

--- src/api/User/me/me.js
+++ src/api/User/me/me.js
@@ -5,13 +5,7 @@ export default {
     me: async (_, __, { request, isAuthenticated }) => {
       isAuthenticated(request);
       const { user } = request;
-      const userProfile = await prisma.user({ id: user.id });
-      // 유저 아이디에 해당되는 posts정보를 가져옴
-      const posts = await prisma.user({ id: user.id }).posts();
-      return {
-        user: userProfile,
-        posts
-      };
+      return await prisma.user({ id: user.id });
     }
-  },
+  }
 };
